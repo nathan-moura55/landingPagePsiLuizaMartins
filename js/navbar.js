7e21de3dda71ddc90f41607c75811d2d054da8d3
Nathan de Moura <nathandemoura7@gmail.com>
2026-07-23 01:08:39 -0300

    ajuste finais de design

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,11 @@
-let ultimaPosicaoScroll = window.scrollY;
-        const navbar = document.querySelector('.navbar');
+document.addEventListener('DOMContentLoaded', () => {
+    let ultimaPosicaoScroll = window.scrollY;
+    const navbar = document.querySelector('.navbar');
 
-        window.addEventListener('scroll', () => {
+    window.addEventListener('scroll', () => {
+        const eDesktopOuPaisagem = window.innerWidth > 1024 || window.matchMedia("(orientation: landscape)").matches;
+
+        if (eDesktopOuPaisagem && navbar) {
             const posicaoAtualScroll = window.scrollY;
 
             if (posicaoAtualScroll > ultimaPosicaoScroll && posicaoAtualScroll > 80) {
@@ -11,4 +15,38 @@ let ultimaPosicaoScroll = window.scrollY;
             }
 
             ultimaPosicaoScroll = posicaoAtualScroll;
+        } else if (navbar) {
+            navbar.classList.remove('ocultar');
+        }
+    });
+
+    const icon = document.getElementById("hamburger-icon");
+    const icon1 = document.getElementById("icon-a");
+    const icon2 = document.getElementById("icon-b");
+    const icon3 = document.getElementById("icon-c");
+    const navMenu = document.getElementById("nav-menu");
+    const blueBg = document.getElementById("blue-bg");
+    const navLinks = document.querySelectorAll(".Navbar-links a");
+
+    function toggleMenu() {
+        if (icon1 && icon2 && icon3 && navMenu && blueBg) {
+            icon1.classList.toggle('a');
+            icon2.classList.toggle('c');
+            icon3.classList.toggle('b');
+            navMenu.classList.toggle('show');
+            blueBg.classList.toggle('slide');
+        }
+    }
+
+    if (icon) {
+        icon.addEventListener('click', toggleMenu);
+    }
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            if (navMenu && navMenu.classList.contains('show')) {
+                toggleMenu();
+            }
         });
+    });
+});
